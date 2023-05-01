60af87191c0e68f5d021389ec10b073ded8bea4b
nbaishev <nbaishev@gmail.com>
2023-05-01 17:33:46 +0600

    v1.0.0

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,6 +8,7 @@ from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+
 from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitleFilter
