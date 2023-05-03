aed476fedc164f07644db33872022b5621dcfabd
nbaishev <nbaishev@gmail.com>
2023-05-03 13:23:07 +0600

    README changed

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,12 +5,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default='False')
 
-ALLOWED_HOSTS = ['*']
+HOSTS_FROM_ENV = [os.getenv('ALLOWED_HOSTS').split(' ')]
+ALLOWED_HOSTS = HOSTS_FROM_ENV
 
 # Application definition
 
